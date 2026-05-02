7c436e3cf2f5522bb873a915204e4e337ff631ed
Lucas Suota <lucassuotass@gmail.com>
2026-05-02 19:02:42 -0300

    fix: updating error caused by non used components that were still in the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,6 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
-import Capabilities from './components/Capabilities';
-import Process from './components/Process';
 import Footer from './components/Footer';
 
 // Global Styles
@@ -59,8 +57,7 @@ function App() {
         <Hero />
         <About />
         <Projects />
-        {/* <Capabilities /> */}
-        {/* <Process /> */}
+
       </main>
 
       <Footer />
